f679a70bb1465b60bc65190273d9399363fe670a
andz1308 <168605268+andz1308@users.noreply.github.com>
2025-12-01 17:38:16 +0700

    update giao diện

---
+++ WebCinema/Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCinema.Models
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
